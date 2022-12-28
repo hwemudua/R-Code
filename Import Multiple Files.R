3c5d8d3a50e6c04e751713ddf451962b4e9579aa
hwemudua <hwemudua@users.noreply.github.com>
2022-12-28 12:36:43 -0500

    Add files via upload

---
+++ Import Multiple Files.R
@@ -0,0 +1,40 @@
+#Import Multiple CSV files and place into a table
+
+# 1.0 LIBRARIES ----
+
+library(tidyverse)
+library(fs)
+
+# 2.0 IMPORT DATA INTO SINGULAR DATASET ----
+file_paths <- "001_read_multiple_files/data/"
+car_data_list <- file_paths %>%
+    dir_ls() %>%
+    map(
+        .f = function(path) {
+            read_csv(
+                path,
+                col_types = cols(
+                    manufacturer = col_character(),
+                    model = col_character(),
+                    displ = col_double(),
+                    year = col_double(),
+                    cyl = col_double(),
+                    trans = col_character(),
+                    drv = col_character(),
+                    cty = col_double(),
+                    hwy = col_double(),
+                    fl = col_character(),
+                    class = col_character()
+                )
+            )
+        }
+    )
+
+# 3.0 BINDING DATA FRAMES ----
+# - bind_rows() : Taught in DS4B 101-R
+
+car_data_tbl <- car_data_list %>%
+    set_names(dir_ls(file_paths)) %>%
+    bind_rows(.id = "file_path")
+
+car_data_tbl
